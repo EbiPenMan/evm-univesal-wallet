424d0e50f57fd9df2b383df645bb4621fd39b934
EbiPenMan <karimi.gamedev@gmail.com>
2024-06-05 22:20:58 +0330

    Added simple core contracts

---
+++ src/Wallet.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+// TODO
+// receive blockchain native token (Eth,Matic,BNB,...)
+// withdraw blockchain native token (Eth,Matic,BNB,...)
+// receive ERC20
+// withdraw ERC20
+// receive ERC721
+// withdraw ERC721
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ReentrancyGuardTransient } from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
+
+contract Wallet is ReentrancyGuardTransient {
+    //  TODO get hash of unique string slot
+    bytes32 constant SLOT = 0;
+
+    address owner;
+
+    event OnReceiveEth(address indexed sender, uint256 amount);
+    event OnWithdrawEth(address indexed recepiant, uint256 amount);
+    event OnChangedOwner(address indexed oldOwner, address indexed newOwner, address indexed caller);
+
+    error FailedWithdraw();
+    error FailedWithdrawCall(bytes);
+    error ZeroAddress();
+    error ZeroAmount();
+
+    modifier OnlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier NotZeroAmount(uint256 val) {
+        if (val == 0) revert ZeroAmount();
+        _;
+    }
+
+    modifier NotZeroAddress(address val) {
+        if (val == address(0)) revert ZeroAddress();
+        _;
+    }
+
+    modifier nonReentrant() {
+        assembly {
+            if tload(SLOT) { revert(0, 0) }
+            tstore(SLOT, 1)
+        }
+        _;
+        assembly {
+            tstore(SLOT, 0)
+        }
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /*
+    Which function is called, fallback() or receive()?
+
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+    receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+    */
+    receive() external payable NotZeroAmount(msg.value) {
+        // if (msg.value == 0) {
+        //     revert ZeroAmount();
+        // }
+        // require(msg.value > 0, "ZeroAmount");
+        emit OnReceiveEth(msg.sender, msg.value);
+    }
+
+    fallback() external payable NotZeroAmount(msg.value) {
+        emit OnReceiveEth(msg.sender, msg.value);
+    }
+
+    /// (2300 gas, throws error)
+    function withdrawTransfer() external OnlyOwner {
+        uint256 balance = address(this).balance;
+        if (balance <= 0) revert ZeroAmount();
+        payable(msg.sender).transfer(balance);
+        emit OnWithdrawEth(msg.sender, balance);
+    }
+
+    ///(2300 gas, returns bool)
+    function withdrawSend() external OnlyOwner {
+        uint256 balance = address(this).balance;
+        if (balance <= 0) revert ZeroAmount();
+        bool res = payable(msg.sender).send(balance);
+        if (!res) revert FailedWithdraw();
+        emit OnWithdrawEth(msg.sender, balance);
+    }
+
+    /// (forward all gas or set gas, returns bool and data)
+    function withdrawCall() external OnlyOwner {
+        uint256 balance = address(this).balance;
+        if (balance <= 0) revert ZeroAmount();
+        (bool res, bytes memory data) = payable(msg.sender).call{ value: balance }(" ");
+        if (!res) revert FailedWithdrawCall(data);
+        emit OnWithdrawEth(msg.sender, balance);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function changeOwner(address newOwner) external NotZeroAddress(newOwner) OnlyOwner {
+        address oldOwner = owner;
+        owner = newOwner;
+        emit OnChangedOwner(oldOwner, newOwner, msg.sender);
+    }
+}
